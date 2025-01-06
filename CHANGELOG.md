b182f4fa9a6839f0fe02474bbfd718155f5ab5e5
alaric <alaric851@gmail.com>
2025-01-06 23:48:54 +0800

    Update CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 
 ## main (2025-01-06)
+- b9a6353 - alaric, 14 seconds ago : 测试一下
 - dd6a3e6 - alaric, 4 seconds ago : 修改
 - e43de8b - alaric, 3 minutes ago : 1. 修改主文件
 - a0d6aef - alaric, 7 seconds ago : init
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+/**
+ *  路由文件
+ */
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
